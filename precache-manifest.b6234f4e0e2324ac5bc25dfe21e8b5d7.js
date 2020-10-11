0c061c6f77e2001f44f49df1d7619d56590cc552
Atala <atala.rchidi@gmail.com>
2020-10-11 14:17:26 -0400

    Updates

---
+++ precache-manifest.b6234f4e0e2324ac5bc25dfe21e8b5d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3718f1bae9b8653be2fae4fbbbc17a",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "83f53052807dabf6625d",
+    "url": "/counter-app/static/css/main.8b8e4e61.chunk.css"
+  },
+  {
+    "revision": "9394425a6b8574bfd50e",
+    "url": "/counter-app/static/js/2.87faeb51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter-app/static/js/2.87faeb51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f53052807dabf6625d",
+    "url": "/counter-app/static/js/main.5b5d0787.chunk.js"
+  },
+  {
+    "revision": "8d3c91def09e322d68af",
+    "url": "/counter-app/static/js/runtime-main.7c4e8537.js"
+  }
+]);
